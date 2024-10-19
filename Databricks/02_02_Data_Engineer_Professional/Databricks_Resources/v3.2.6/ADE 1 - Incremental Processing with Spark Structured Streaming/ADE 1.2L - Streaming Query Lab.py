0fecd1e78290eacd11209c04e5ff70826aeebfe5
thuanthongtra@gmail.com <thuanthongtra@gmail.com>
2024-10-19 05:12:07 +0000

    Add W1

--- Databricks/02_02_Data_Engineer_Professional/Databricks_Resources/v3.2.6/ADE 1 - Incremental Processing with Spark Structured Streaming/ADE 1.2L - Streaming Query Lab.py
+++ Databricks/02_02_Data_Engineer_Professional/Databricks_Resources/v3.2.6/ADE 1 - Incremental Processing with Spark Structured Streaming/ADE 1.2L - Streaming Query Lab.py
@@ -45,8 +45,14 @@
 # COMMAND ----------
 
 # TODO
-df = (spark.FILL_IN
+df = (spark
+      .readStream
+      .format("delta")
+      .load(DA.paths.sales)
 )
+
+print("Is the dataframe streaming:", df.isStreaming)
+df.printSchema()
 
 # COMMAND ----------
 
@@ -72,8 +78,12 @@ DA.validate_1_1(df)
 # COMMAND ----------
 
 # TODO
-coupon_sales_df = (df.FILL_IN
-)
+from pyspark.sql.functions import col, explode
+
+coupon_sales_df = (df
+                   .withColumn("items", explode(col("items")))
+                   .filter(col("items.coupon").isNotNull())
+                  )
 
 # COMMAND ----------
 
@@ -106,7 +116,15 @@ DA.validate_2_1(coupon_sales_df.schema)
 coupons_checkpoint_path = f"{DA.paths.checkpoints}/coupon-sales"
 coupons_output_path = f"{DA.paths.working_dir}/coupon-sales/output"
 
-coupon_sales_query = (coupon_sales_df.FILL_IN)
+coupon_sales_query = (coupon_sales_df
+                      .writeStream
+                      .outputMode("append")
+                      .format("delta")
+                      .queryName("coupon_sales")
+                      .trigger(processingTime="1 second")
+                      .option("checkpointLocation", coupons_checkpoint_path)
+                      .start(coupons_output_path)
+                      )
 
 DA.block_until_stream_is_ready(coupon_sales_query)
 
@@ -132,12 +150,14 @@ DA.validate_3_1(coupon_sales_query)
 # COMMAND ----------
 
 # TODO
-query_id = coupon_sales_query.FILL_IN
+query_id = coupon_sales_query.id
+print(query_id)
 
 # COMMAND ----------
 
 # TODO
-query_status = coupon_sales_query.FILL_IN
+query_status = coupon_sales_query.status
+print(query_status)
 
 # COMMAND ----------
 
@@ -161,7 +181,8 @@ DA.validate_4_1(query_id, query_status)
 # COMMAND ----------
 
 # TODO
-coupon_sales_query.FILL_IN
+coupon_sales_query.stop()
+coupon_sales_query.awaitTermination()
 
 # COMMAND ----------
 
@@ -183,6 +204,7 @@ DA.validate_5_1(coupon_sales_query)
 # COMMAND ----------
 
 # TODO
+display(spark.read.format("delta").load(coupons_output_path))
 
 # COMMAND ----------
 
