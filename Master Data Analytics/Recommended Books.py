21d54201fc85eca19019320df5cdc24d0796d949
thuanthongtra@gmail.com <thuanthongtra@gmail.com>
2024-10-27 03:25:42 +0000

    W2-3

--- Master Data Analytics/Recommended Books.py
+++ Master Data Analytics/Recommended Books.py
@@ -1,4 +1,23 @@
 # Databricks notebook source
-W1 - Think Fast --> Selft Service
-W2.1 - Bulletproff Problem Solving
-W2.2 - Structure Thinking --> Logic Tree
+# MAGIC %md
+# MAGIC # W1 
+# MAGIC - Think Fast --> Selft Service
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # W2
+# MAGIC - Bulletproff Problem Solving
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # W2&3 
+# MAGIC - Structure Thinking --> Logic Tree
+# MAGIC - Qlik View Your Business --> How to decide Metrics, Drivers, Example of Cores Structure for Sales
+# MAGIC - garyfox.co/blog --> Business Model
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
